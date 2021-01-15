db1051f766b6fe13bd8c92d0bb2251f152b99116
horizon <1713113814@qq.com>
2021-01-15 09:23:15 +0800

    first commit!

---
+++ src/main/java/com/wmq/lecture/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.wmq.lecture.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author horizon
+ * 配置前后端口跨域可以进行访问
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    /**
+     * 页面跨域访问Controller过滤
+     *
+     * @return
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("POST","GET","DELETE")
+                .allowedOrigins("*");
+    }
+}
